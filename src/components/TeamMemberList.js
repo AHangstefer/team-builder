988f98545cc3d031c47c70414ad25b0dad1ddf81
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-12 22:30:17 -0400

    added styled component

--- src/components/TeamMemberList.js
+++ src/components/TeamMemberList.js
@@ -1,16 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
+const DIV = styled.div`
+ display: flex;
+ justify-content: center;
+ white-space: nowrap;
+ 
+ 
+`;
+
+const P = styled.p`
+ padding-left: 4%;
+ padding-right: 4%;
+`;
 
 
 const TeamMemberList = props => {
     return(
       <div className = "new-member">
         {props.members.map((m, i) =>(
-         <div className = "m" key = {1}>
-            <h2>{m.name}</h2>
-            <p>{m.email}</p>
+         <DIV className = "m" key = {1}>
+            <h4>{m.name}</h4>
+            <P>{m.email}</P>
             <p>{m.roll}</p>
-            </div>
+            </DIV>
         ))}
       </div>
     );
